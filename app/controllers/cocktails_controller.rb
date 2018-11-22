bb7ef7cd0ff07a946ef4fdf6d6cd3258022af03f
Douglas Bay <douglasbays@gmail.com>
2018-11-22 15:12:32 +0100

    Routes and Controller updates

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,28 @@
+class CocktailsController < ApplicationController
+   def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktails_path
+  end
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
